8bf07907b23d2750cbb56e94919ebf5f9504e43c
S56TS <soundtechnic@gmail.com>
2024-02-24 23:06:42 +0100

    Update SimplexLogic.tcl
    * added gpio init to frn tcl script

--- src/svxlink/svxlink/SimplexLogic.tcl
+++ src/svxlink/svxlink/SimplexLogic.tcl
@@ -18,7 +18,6 @@ if {$logic_name != [namespace tail [namespace current]]} {
   return;
 }
 
-
 #
 # A variable used to time for restart timer for FRN module [sec].
 #
@@ -28,8 +27,18 @@ variable frn_timer_sec 0;
 #
 # A variable used to time for WDS watchdog signal [min].
 #
-variable wds_timer_min;
+variable wds_timer_min 0;
 
+#
+# User variables
+#
+set led_state 0;
+set gpio_state(0) 0;
+set gpio_state(1) 0;
+set gpio_state(2) 0;
+set gpio_state(3) 0;
+set gpio_state(4) 0;
+set gpio_str_bsy "echo: write error: Device or resource busy"
 
 #
 # Executed when the SvxLink software is started
@@ -40,6 +49,16 @@ proc startup {} {
   global frn_time_sec;
   variable frn_timer_sec;
   variable wds_timer_min;
+  variable gpio_state;
+  variable gpio_str_bsy;
+  set res(0) 0;
+  set res(1) 0;
+  set res(2) 0;
+  set res(3) 0;
+  set res(4) 0;
+  set GPIO_NOINIT 0:
+  set GPIO_INITOK 1;
+  set GPIO_INITER 2;
   append func $logic_name "::checkPeriodicIdentify";
   Logic::addMinuteTickSubscriber $func;
   Logic::startup;
@@ -47,6 +66,111 @@ proc startup {} {
   # run FRN module
   set wds_timer_min $wds_time_min;
   set frn_timer_sec $frn_time_sec;
+
+  # initilize led's
+  set res(0) [catch {
+    exec echo 17 > /sys/class/gpio/export
+  } res_str(0)]
+
+  set res(1) [catch {
+    exec echo 22 > /sys/class/gpio/export
+  } res_str(1)]
+
+  set res(2) [catch {
+    exec echo 27 > /sys/class/gpio/export
+  } res_str(2)]
+
+  set res(3) [catch {
+    exec echo 23 > /sys/class/gpio/export
+  } res_str(3)]
+
+  set res(4) [catch {
+    exec echo 24 > /sys/class/gpio/export
+  } res_str(4)]
+
+
+  for {set i 0} {$i < 5} {incr i} {
+    if {$res($i) != 0} {
+        if {[string equal $res_str($i) $gpio_str_bsy] == 1} {
+            set gpio_state($i) $GPIO_INITOK;
+        } else {
+            set gpio_state($i) $GPIO_INITER;
+        }
+    } else {
+            set gpio_state($i) $GPIO_INITOK;
+    }
+  }
+
+  if {$gpio_state(0) == $GPIO_INITOK} {
+    if {[catch {exec sudo echo out > /sys/class/gpio/gpio17/direction} result] == 0} {
+      if {[catch {exec echo 0 > /sys/class/gpio/gpio17/value} result] != 0} {
+        set gpio_state(0) $GPIO_INITER;
+        puts "GPIO0 value init error"
+      }
+    } else {
+     set gpio_state(0) $GPIO_INITER;
+      puts "GPIO0 direction init error, error: $result"
+    }
+  } else {
+    set gpio_state(0) $GPIO_INITER;
+    puts "GPIO0 export init error"
+  }
+
+  if {$gpio_state(1) == $GPIO_INITOK} {
+    if {[catch {exec echo out > /sys/class/gpio/gpio22/direction} result] == 0} {
+      if {[catch {exec echo 0 > /sys/class/gpio/gpio22/value} result] != 0} {
+        set gpio_state(1) $GPIO_INITER;
+        puts "GPIO1 value init error"
+      }
+    } else {
+     set gpio_state(1) $GPIO_INITER;
+      puts "GPIO1 direction init error"
+    }
+  } else {
+    set gpio_state(1) $GPIO_INITER;
+    puts "GPIO1 export init error"
+  }
+
+  if {$gpio_state(2) == $GPIO_INITOK} {
+    if {[catch {exec echo out > /sys/class/gpio/gpio27/direction} result] == 0} {
+      if {[catch {exec echo 0 > /sys/class/gpio/gpio27/value} result] != 0} {
+        set gpio_state(2) $GPIO_INITER;
+        puts "GPIO2 value init error"
+      }
+    } else {
+     set gpio_state(2) $GPIO_INITER;
+      puts "GPIO2 direction init error"
+    }
+  } else {
+    set gpio_state(2) $GPIO_INITER;
+    puts "GPIO2 export init error"
+  }
+
+  if {$gpio_state(3) == $GPIO_INITOK} {
+    if {[catch {exec echo out > /sys/class/gpio/gpio23/direction} result] == 0} {
+      if {[catch {exec echo 0 > /sys/class/gpio/gpio23/value} result] != 0} {
+        set gpio_state(3) $GPIO_INITER;
+        puts "GPIO2 value init error"
+      }
+    } else {
+     set gpio_state(3) $GPIO_INITER;
+      puts "GPIO2 direction init error"
+    }
+  } else {
+    set gpio_state(3) $GPIO_INITER;
+    puts "GPIO2 export init error"
+  }
+
+  if {$gpio_state(4) == $GPIO_INITOK} {
+    if {[catch {exec echo in > /sys/class/gpio/gpio24/direction} result] == 0} {
+    } else {
+     set gpio_state(4) $GPIO_INITER;
+      puts "GPIO3 direction init error"
+    }
+  } else {
+    set gpio_state(4) $GPIO_INITER;
+    puts "GPIO3 export init error"
+  }
 }
 
 
@@ -189,7 +313,7 @@ proc every_minute {} {
 
   if {$wds_timer_min > 0} {
     set wds_timer_min [expr $wds_timer_min - 1];
-    if {wds_timer_min == 0} {
+    if {$wds_timer_min == 0} {
       puts "SimplexLogic: WDS signal"
       set wds_timer_min  $wds_time_min;
     }
@@ -205,15 +329,32 @@ proc every_second {} {
   global active_module;
   global frn_time_sec;
   variable frn_timer_sec;
+  variable led_state;
+  variable gpio_state;
+  set GPIO_NOINIT 0:
+  set GPIO_INITOK 1;
+  set GPIO_INITER 2;
 
-  if {$frn_timer_sec > 0} {
-    set frn_timer_sec [expr $frn_timer_sec - 1];
-    if {$frn_timer_sec == 0} {
-      if {$active_module == ""} {
+  if {$active_module == ""} {
+    if {$frn_timer_sec > 0} {
+      set frn_timer_sec [expr $frn_timer_sec - 1];
+      if {$frn_timer_sec == 0} {
         puts "SimplexLogic: FRN restart timer elapsed. No active module, activating FRN module."
+        set frn_timer_sec $frn_time_sec;
         injectDtmf 2#;
       }
-      set frn_timer_sec $frn_time_sec;
+    }
+  } else {
+    set frn_timer_sec $frn_time_sec;
+  }
+
+  if {$gpio_state(1) == $GPIO_INITOK} {
+    if {$led_state == 0} {
+      set led_state 1;
+      exec echo 1 >/sys/class/gpio/gpio22/value;
+    } else {
+      set led_state 0;
+      exec echo 0 >/sys/class/gpio/gpio22/value;
     }
   }
   Logic::every_second;
@@ -224,7 +365,18 @@ proc every_second {} {
 # Executed each time the transmitter is turned on or off
 #
 proc transmit {is_on} {
-  Logic::transmit $is_on;
+ variable gpio_state;
+ set GPIO_NOINIT 0:
+ set GPIO_INITOK 1;
+ set GPIO_INITER 2;
+
+ Logic::transmit $is_on;
+
+ if {$is_on == 0 && $gpio_state(0) == $GPIO_INITOK} {exec echo 0 >/sys/class/gpio/gpio17/value}
+ if {$is_on == 1 && $gpio_state(0) == $GPIO_INITOK} {exec echo 1 >/sys/class/gpio/gpio17/value}
+
+# if {$is_on == 1} {puts "Tx Led: ON"}
+# if {$is_on == 0} {puts "Tx Led: OFF"}
 }
 
 
@@ -232,7 +384,18 @@ proc transmit {is_on} {
 # Executed each time the squelch is opened or closed
 #
 proc squelch_open {rx_id is_open} {
-  Logic::squelch_open $rx_id $is_open;
+ variable gpio_state;
+ set GPIO_NOINIT 0:
+ set GPIO_INITOK 1;
+ set GPIO_INITER 2;
+
+ Logic::squelch_open $rx_id $is_open;
+
+ if {$is_open == 0 && $gpio_state(2) == $GPIO_INITOK} {exec echo 0 >/sys/class/gpio/gpio27/value}
+ if {$is_open == 1 && $gpio_state(2) == $GPIO_INITOK} {exec echo 1 >/sys/class/gpio/gpio27/value}
+
+# if {$is_open == 1} {puts "Rx Led: ON"}
+# if {$is_open == 0} {puts "Rx Led: OFF"}
 }
 
 
